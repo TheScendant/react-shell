47f2bcd0a3df1ad6a56240dc55d7410541985458
TheScendant <hayden518@gmail.com>
2021-03-22 20:12:27 -0600

    Initial config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { mount, shallow } from 'enzyme';
+import { Provider } from 'react-redux'
+// import store from './store'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('mounts learn react link', () => {
+  const app = mount(<App />);
+  const header = app.find(".App-header");
+  expect(header).toHaveLength(1)
 });
